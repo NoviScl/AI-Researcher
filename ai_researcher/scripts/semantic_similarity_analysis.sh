87eaf30030b0635a11150767025be74efff5d40e
NoviScl <sichenglei1125@gmail.com>
2024-06-26 13:21:08 -0700

    semantic similarity

---
+++ ai_researcher/scripts/semantic_similarity_analysis.sh
@@ -0,0 +1,6 @@
+cache_names=("bias" "coding" "factuality" "math" "multilingual" "safety" "uncertainty")
+
+for cache_name in "${cache_names[@]}"; do
+    echo "Running analyze_ideas2.py with cache_name: $cache_name"
+    python3 src/analyze_ideas2.py --cache_name "$cache_name" > logs/semantic_similarity_"$cache_name".log 2>&1
+done
