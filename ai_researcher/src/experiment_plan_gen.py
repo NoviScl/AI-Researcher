95e95202b7675278dd8e9fa7d75691b55ea059fc
NoviScl <sichenglei1125@gmail.com>
2024-06-30 16:07:45 -0700

    revamp experiment plan gen

--- ai_researcher/src/experiment_plan_gen.py
+++ ai_researcher/src/experiment_plan_gen.py
@@ -14,23 +14,23 @@ random.seed(2024)
 def plan_generation_method(method, idea, demo_examples, topic_description, openai_client, model, seed):
     ## forumate an idea from a paragraph into a full experiment plan based on our template
 
-    prompt = "You are an expert researcher in Natural Language Processing and your job is to expand a brief and vague project idea into a detailed experiment plan so that your student can follow the steps and execute the full project. I will provide you with an idea on the topic of: " + topic_description + ".\n\n"
+    prompt = "You are an expert researcher in Natural Language Processing and your job is to expand a brief project idea into a full project proposal with detailed experiment plans so that your students can follow the steps and execute the full project. I will provide you with an idea on the topic of: " + topic_description + ".\n\n"
     prompt += "The idea is:\n" + format_plan_json(idea) + "\n"
-    # prompt += "Here are some relevant papers that are relevant to the idea:\n" + format_papers_for_printing(grounding_papers) + "\n\n"
     prompt += "Now you should come up with the full experiment plan covering:\n"
     prompt += "1. Title: A concise statement of the main research question to be used as the paper title.\n"
     prompt += "2. Problem Statement: Clearly define the problem your research intends to address. Explain clearly why this problem is interesting and important.\n"
-    prompt += "3. Motivation: Explain why existing methods are not good enough to solve the problem, and explain the inspiration behind the new proposed method. You should also motivate why the proposed method would work better than existing baselines on the problem.\n"
-    prompt += "4. Proposed Method: Explain how the proposed method works, describe all the essential steps.\n"
-    prompt += "5. Step-by-Step Experiment Plan: Break down every single step of the experiments, make sure every step is executable. Cover all essential details such as the datasets, models, and metrics to be used. If the project involves prompting, give some example prompts for each step.\n"
-    prompt += "6. Test Case Examples: Give two concrete examples. The first example should show how the baseline method fails on the test case. If there are multiple baselines, give examples for all of them. The second example should show how the proposed method succeeds on the test case. For each test cases, include the input (test example and the full prompt) and the expected output. You should also provide an explanation for why the outputs from the proposed prompt are better. If the proposed method has multiple steps, break them down into intermediate steps.\n"
-    prompt += "7. Fallback Plan: Propose some alternative plans for what should the students do if the proposed method didn't manage to satisfy the success criteria. For example, you can suggest additional analysis to help debug why the proposed method didn't work, which could inform alternative new methods, or just turn the project into an analysis paper instead by offering some interesting ablation and insights. Write a coherent paragraph rather than a list of bullet points.\n"
-    prompt += "The experiment plan should not include any background introduction (e.g., you can skip the literature review, paper writing tips, and ethical discussion). Just give instructions on the experiments.\n"
+    prompt += "3. Motivation: Explain why existing methods (both classic ones and recent ones) are not good enough to solve the problem, and explain the inspiration behind the new proposed method. You should also motivate why the proposed method would work better than existing baselines on the problem.\n"
+    prompt += "4. Proposed Method: Explain how the proposed method works, describe all the steps. Make sure every step is clearly described and feasible to implement.\n"
+    prompt += "5. Step-by-Step Experiment Plan: Break down every single step of the experiments, make sure every step is executable. Cover all essential details such as the datasets, models, and metrics to be used. If the project involves prompting, give example prompts for each step.\n"
+    prompt += "6. Test Case Examples: Give two concrete examples. The first example should show how the baseline method fails on the test case. If there are multiple baselines, give examples for all of them. The second example should show how the proposed method succeeds on the test case. For each test case, include the input (test example and the full prompt) and the expected output. You should also provide an explanation for why the outputs from the proposed prompt are better. If the proposed method has multiple steps, break them down into intermediate steps.\n"
+    prompt += "7. Fallback Plan: Propose some alternative plans for what should the students do if the proposed method didn't manage to satisfy the success criteria. For example, you can suggest additional analysis to help debug why the proposed method didn't work, which could inform alternative new methods, or turn the project into an analysis paper instead by offering some interesting ablation and insights. Write a coherent paragraph rather than a list of bullet points.\n"
+    prompt += "The experiment plan should not include any background introduction (you can skip the literature review, paper writing tips, and ethical discussion). Just give instructions on the experiments.\n"
     if method == "prompting":
-        prompt += "When designing experiments, try to avoid pre-training models from scratch and instead prefer prompting-based methods. In rare cases, finetuning small open-source language models is also acceptable. Try to avoid large-scale human evaluation or human data collection, which is time-consuming and expensive.\n"
-    prompt += "Below is a few examples of how the full experiment plans should look like:\n"
+        prompt += "When designing experiments, avoid pretraining new models from scratch and instead prefer prompting-based methods. In rare cases, finetuning small open-source language models is also acceptable. Avoid large-scale human evaluation or human data collection, which is time-consuming and expensive. In general, you should assume that the students have access to abundant black-box LLM API credits (e.g., GPT, Claude, and Gemini) but very limited GPU resources for running training jobs (but running inferences on open-source models is affordable).\n"
+    prompt += "Be consistent in your methodology and experiment design, for example, if you will use black-box LLM APIs such as GPT and Claude for your experiments, then you shouldn't propose any experiments that require white-box model weights or data access and you should edit them accordingly to follow the black-box assumptions.\n"
+    prompt += "Below are a few examples of how the full experiment plans should look like:\n"
     prompt += demo_examples + "\n\n"
-    prompt += "Now please write down your experiment plan in JSON format (keys should be the section names, just like the above examples). Make sure to be as detailed as possible especially so that a student can directly follow the plan to implement the experiments."
+    prompt += "Now please write down your experiment plan in JSON format (keys should be the section names, just like the above examples). Make sure to be as detailed as possible so that a student can directly follow the plan to implement the project."
     
     prompt_messages = [{"role": "user", "content": prompt}]
     response, cost = call_api(openai_client, model, prompt_messages, temperature=0., max_tokens=4096, seed=seed, json_output=True)
@@ -74,7 +74,7 @@ if __name__ == "__main__":
         with open("prompts/experiment_plan_examples_finetuning.txt", "r") as f:
             demo_examples = f.read().strip()
     
-    with open(cache_dir + "ideas_1k_dedup/" + args.cache_name + ".json") as f:
+    with open(cache_dir + "ideas_1k_claude3-5_dedup/" + args.cache_name + ".json") as f:
         idea_file = json.load(f)
     topic_description = idea_file["topic_description"]
     all_ideas = idea_file["ideas"]
