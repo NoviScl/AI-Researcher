0ea74ecd7f4b9c03014dc96b2f8b2d17cb0ca9bf
NoviScl <sichenglei1125@gmail.com>
2024-07-28 19:54:53 -0700

    style transfer

--- ai_researcher/scripts/style_transfer.sh
+++ ai_researcher/scripts/style_transfer.sh
@@ -22,9 +22,9 @@
 
 python3 src/style_transfer.py \
  --engine claude-3-5-sonnet-20240620 \
- --cache_dir "../Human_Ideas_Txt_More/" \
- --idea_name "IdeaGeneration_Abe.txt" \
- --format "txt" \
- --processed_cache_dir "../Human_Ideas_Txt_More_Processed/" >> logs/style_transfer_human_ideas_more.log 2>&1
+ --cache_dir "../AI_Human_Ideas/" \
+ --idea_name "all" \
+ --format "json" \
+ --processed_cache_dir "../AI_Human_Ideas_Txt/" >> logs/style_transfer_ai_human_ideas.log 2>&1
 
 
